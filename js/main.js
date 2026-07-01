a7e475c203505ca687a41ad7f4056ad82544771e
Boris Boulogne <boulogneboris@gmail.com>
2026-07-01 03:31:55 +0200

    *** RESPONSIVE IN PROGRESS ***
    _ responsive okay until 575px, still need to verify the smaller screens
    _ ellipse animation on powders for desktop
    _ scrollable powder section under 1000px done
    _ scrollable community videos done
    _ clouds animation done
    
    To do :
    _ verify under 575px

--- js/main.js
+++ js/main.js
@@ -348,12 +348,12 @@ function moveElements() {
     // document.querySelector('.product-item[data-pos="2"]').style.translate = "-100% 0 -400px";
     // document.querySelector('.product-item[data-pos="3"]').style.translate = "0 0 0"
 
-    document.querySelector('.product-item[data-pos="1"]').setAttribute('data-pos', 5)
-    document.querySelector('.product-item[data-pos="2"]').setAttribute('data-pos', 6)
-    document.querySelector('.product-item[data-pos="3"]').setAttribute('data-pos', 4)
-    document.querySelector('.product-item[data-pos="4"]').setAttribute('data-pos', 2)
-    document.querySelector('.product-item[data-pos="5"]').setAttribute('data-pos', 3)
-    document.querySelector('.product-item[data-pos="6"]').setAttribute('data-pos', 1)
+    document.querySelector('.product-item-CTN[data-pos="1"]').setAttribute('data-pos', 5)
+    document.querySelector('.product-item-CTN[data-pos="2"]').setAttribute('data-pos', 6)
+    document.querySelector('.product-item-CTN[data-pos="3"]').setAttribute('data-pos', 4)
+    document.querySelector('.product-item-CTN[data-pos="4"]').setAttribute('data-pos', 2)
+    document.querySelector('.product-item-CTN[data-pos="5"]').setAttribute('data-pos', 3)
+    document.querySelector('.product-item-CTN[data-pos="6"]').setAttribute('data-pos', 1)
     console.log('New');
 
 }
